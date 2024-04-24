951acf5b9005c78ad32d7a8522251e25699b8114
Денис Касперович <kasperovich.deonis@gmail.com>
2024-04-24 21:31:29 +0300

    CRA training (card)

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return <div className="wrapper">
+    <main>
+      <div className="card">
+        <img src="" alt=""/>
+        <div className="content">
+          <h1>Headline</h1>
+          <p>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</p>
+          {/*<a href={"#"} className="seeMore">see more</a>*/}
+          <button>save</button>
+        </div>
+      </div>
+    </main>
+  </div>
+}
+
+export default App;
