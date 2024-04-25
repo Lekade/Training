9ab5e21a27e640f2da06fff3692e5055ad624b93
Денис Касперович <kasperovich.deonis@gmail.com>
2024-04-25 16:02:11 +0300

    global styles

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,33 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  body{
+    width:100%;
+    height:100%;
+    background-color: #01010101;
+  }
+
+  *,
+  :before,
+  :after{
+    box-sizing:border-box;
+  }
+
+  body, h1, h2, h3, h4, h5, h6, p{
+    margin:0;
+  }
+
+  html{
+    height: 100%;
+    font-size: calc(100vw/1920*10);
+  }
+  
+  .wrapper{
+    min-height:100vh;
+    position: relative;
+    width:100%;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+  }
+`
