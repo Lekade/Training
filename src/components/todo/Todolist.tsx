334921cb7ff45406a8e4998e8f3d5952dec82f30
Денис Касперович <kasperovich.deonis@gmail.com>
2024-06-16 21:45:41 +0300

    stylization and throwing of props

--- src/components/todo/Todolist.tsx
+++ src/components/todo/Todolist.tsx
@@ -1,6 +1,10 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import React from 'react';
 import {FilterValuesType} from './TodolistApp';
 import './TodolistApp.css';
+import Button from "./button/Button";
+import AddItemForm from "./addItemForm/AddItemForm";
+import styled from "styled-components";
+import {TitleInput} from "./titleInput/TitleInput";
 
 export type TaskType = {
     idTask: string
@@ -17,29 +21,20 @@ type TodolistPropsType = {
     addNewTask: (inputValue: string, todolistId: string) => void
     changeStatusTask: (idTask: string, isDone: boolean, todolistId: string) => void
     activeFilter: FilterValuesType
-    changeFilter: (value:FilterValuesType, todolistId: string) => void
+    changeFilter: (value: FilterValuesType, todolistId: string) => void
+    deleteTodolist: (todolistId: string) => void
+    changeTitleTodolist: (title: string, todolistId: string) => void
+    changeTitleTask: (title: string, idTask: string, todolistId: string) => void
 }
 
 export function Todolist(props: TodolistPropsType) {
 
-    const [inputValue, setInputValue] = useState('')
-    const [error, setError] = useState(false)
-
-    const changeInputValue = (e: ChangeEvent<HTMLInputElement>) => {
-        error && setError(false)
-        setInputValue(e.currentTarget.value)
+    const addItemTasks = (inputValue: string) => {
+        props.addNewTask(inputValue.trim(), props.todolistId)
     }
-
-    const addTask = () => {
-        if(inputValue.trim()){
-            props.addNewTask(inputValue.trim(), props.todolistId)
-            setInputValue('')
-        }else {
-            setError(true)
-        }
+    const changeTitleTodolistHandler = (title: string) => {
+        props.changeTitleTodolist(title, props.todolistId)
     }
-
-    const onInputKeyDown = (e: KeyboardEvent<HTMLInputElement>) => e.key === "Enter" && addTask()
 
     let tasksForTodolist = props.tasks;
 
@@ -51,40 +46,83 @@ export function Todolist(props: TodolistPropsType) {
     }
 
 
-    return <div>
-        <h3>{props.title}</h3>
-        <div>
-            <input className={error ? 'error' : ''} value={inputValue} onChange={changeInputValue} onKeyDown={onInputKeyDown}/>
-            <button onClick={addTask}>+</button>
-            {error && <div className='error-message'>The field is required</div>}
-        </div>
+    return <TodolistStyled>
+        <DeleteTodolist><Button onClick={() => props.deleteTodolist(props.todolistId)}>X</Button></DeleteTodolist>
+        <TitleInput title={props.title} onChangeTitle={changeTitleTodolistHandler}/>
+        <AddItemForm addItem={addItemTasks}/>
         <ul>
             {
-                tasksForTodolist.map(t => <li key={t.idTask}>
-                    <input type="checkbox" onChange={(e) => props.changeStatusTask(t.idTask, e.currentTarget.checked, props.todolistId)}
-                           checked={t.isDone}/>
-                    <span>{t.title}</span>
-                    <button onClick={() => props.removeTask(t.idTask, props.todolistId)}>x</button>
-                </li>)
+                tasksForTodolist.map(t => {
+                    const changeTitleTaskHandler = (title: string)=>{
+                        props.changeTitleTask(title, t.idTask, props.todolistId)
+                    }
+
+                        return (
+                            <Task key={t.idTask}>
+                                <input type="checkbox"
+                                       onChange={(e) => props.changeStatusTask(t.idTask, e.currentTarget.checked, props.todolistId)}
+                                       checked={t.isDone}/>
+                                <TitleInput title={t.title} onChangeTitle={changeTitleTaskHandler}/>
+                                <Button onClick={() => props.removeTask(t.idTask, props.todolistId)}>X</Button>
+                            </Task>
+                        )}
+                )
             }
         </ul>
-        <button onClick={() => props.removeAllTasks(props.todolistId)}>DELETE ALL TASKS</button>
+        <Button onClick={() => props.removeAllTasks(props.todolistId)}>DELETE ALL TASKS</Button>
         <div>
-            <button className={props.activeFilter === 'all' ? 'active-filter' : ''} onClick={() => {
-                props.changeFilter("all", props.todolistId)
-            }}>
-                All
-            </button>
-            <button className={props.activeFilter === 'active' ? 'active-filter' : ''} onClick={() => {
-                props.changeFilter("active", props.todolistId)
-            }}>
-                Active
-            </button>
-            <button className={props.activeFilter === 'completed' ? 'active-filter' : ''} onClick={() => {
-                props.changeFilter("completed", props.todolistId)
-            }}>
-                Completed
-            </button>
+            <Button className={props.activeFilter === 'all' ? 'active-filter' : ''}
+                    onClick={() => {
+                        props.changeFilter("all", props.todolistId)
+                    }}>All</Button>
+            <Button className={props.activeFilter === 'active' ? 'active-filter' : ''}
+                    onClick={() => {
+                        props.changeFilter("active", props.todolistId)
+                    }}>Active</Button>
+            <Button className={props.activeFilter === 'completed' ? 'active-filter' : ''}
+                    onClick={() => {
+                        props.changeFilter("completed", props.todolistId)
+                    }}>Completed</Button>
         </div>
-    </div>
+    </TodolistStyled>
 }
+
+
+const TodolistStyled = styled.li`
+  display: block;
+  min-width: 300px;
+  min-height: 200px;
+  background-color: cadetblue;
+  padding: 30px;
+  margin: 0;
+  border-radius: 10px;
+  position: relative;
+  list-style-type: none;
+
+  ul {
+    padding-top: 30px;
+    padding-bottom: 30px;
+    padding-left: 0;
+  }
+`
+const DeleteTodolist = styled.div`
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  Button {
+    width: 100%;
+    height: 100%;
+    background-color: transparent;
+    font-size: 25px;
+    border: none;
+  }
+`
+
+const Task = styled.li`
+  list-style-type: none;
+  display: flex;
+
+`
